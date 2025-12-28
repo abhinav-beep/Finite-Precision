6f06f03358e38659c876985e23c176064313979d
Abhinav <abhinavravi.shankar.s.112@kalvium.community>
2025-12-28 21:31:22 +0530

    Implement cosine function via Taylor series with controlled precision

--- src/math/TaylorCos.java
+++ src/math/TaylorCos.java
@@ -1,4 +1,41 @@
 package math;
 
+import util.PrecisionContext;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+
 public class TaylorCos {
+
+    public static BigDecimal cos(BigDecimal x, PrecisionContext pc) {
+
+        MathContext mc = pc.mc();
+
+        BigDecimal sum = BigDecimal.ONE;   // first term: 1
+        BigDecimal term = BigDecimal.ONE;  // current term
+        BigDecimal x2 = x.multiply(x, mc);
+
+        int n = 1;
+
+        while (n <= pc.maxIterations()) {
+
+            // term *= -x^2 / ((2n-1)(2n))
+            BigDecimal denominator = BigDecimal.valueOf(2L * n - 1)
+                    .multiply(BigDecimal.valueOf(2L * n), mc);
+
+            term = term.multiply(x2, mc)
+                    .divide(denominator, mc)
+                    .negate();
+
+            sum = sum.add(term, mc);
+
+            if (term.abs().compareTo(pc.epsilon()) < 0) {
+                break;
+            }
+
+            n++;
+        }
+
+        return sum;
+    }
 }
--- src/math/TaylorSin.java
+++ src/math/TaylorSin.java
@@ -1,4 +1,41 @@
 package math;
 
+import util.PrecisionContext;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+
 public class TaylorSin {
+
+    public static BigDecimal sin(BigDecimal x, PrecisionContext pc) {
+
+        MathContext mc = pc.mc();
+
+        BigDecimal sum = x;          // first term: x
+        BigDecimal term = x;         // current term
+        BigDecimal x2 = x.multiply(x, mc);
+
+        int n = 1;
+
+        while (n <= pc.maxIterations()) {
+
+            // term *= -x^2 / ((2n)(2n+1))
+            BigDecimal denominator = BigDecimal.valueOf(2L * n)
+                    .multiply(BigDecimal.valueOf(2L * n + 1), mc);
+
+            term = term.multiply(x2, mc)
+                    .divide(denominator, mc)
+                    .negate();
+
+            sum = sum.add(term, mc);
+
+            if (term.abs().compareTo(pc.epsilon()) < 0) {
+                break;
+            }
+
+            n++;
+        }
+
+        return sum;
+    }
 }
